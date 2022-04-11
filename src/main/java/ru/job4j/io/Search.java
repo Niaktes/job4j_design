0295be3606e161b26b565da031002ad8e184b6c6
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2022-04-11 21:06:19 +0300

    2.2.1.5 Arguments validation fixed.

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,8 +9,12 @@ import java.util.function.Predicate;
 public class Search {
 
 	public static void main(String[] args) throws IOException {
-		argumentsValidation(args);
+		if (args.length != 2) {
+			throw new IllegalArgumentException("Root folder or file extension was not specified. Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION");
+		}
 		Path path = Path.of(args[0]);
+		String extension = args[1];
+		argumentsValidation(path, extension);
 		search(path, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
 	}
 	
@@ -21,17 +24,12 @@ public class Search {
 		return searcher.getPaths();
 	}
 	
-	private static void argumentsValidation(String[] args) {
-		if (args.length != 2) {
-			throw new IllegalArgumentException("Root folder or file extension was not specified. Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION");
-		}
-		File file = new File(args[0]);
-		if (!file.isDirectory()) {
+	private static void argumentsValidation(Path path, String extension) {
+		if (!path.toFile().isDirectory()) {
 			throw new IllegalArgumentException("Invalid root folder. Please check first entered argument.");
 		}
-		String extension = args[1];
 		if (!extension.startsWith(".")) {
-			throw new IllegalArgumentException("Invalid extension. Please check second entered argument.");
+			throw new IllegalArgumentException("Invalid extension. Second entered argument need to be started with \".\"-symbol.");
 		}
 	}
 
