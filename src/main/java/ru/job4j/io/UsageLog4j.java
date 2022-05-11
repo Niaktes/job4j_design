93eb7bd78b6b27033ff889a825ebf15e9666e8d2
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2022-05-11 20:13:30 +0300

    2.2.3.3 slf4j variables template used.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,11 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        char c = 'c';
+        byte b = 62;
+        short s = 22;
+        int i = 1;
+        long l = 123;
+        float f = 1.1f;
+        double d = 1.0d;
+        boolean bool = true;
+
+        LOG.debug("char = {}, byte = {}, short = {}, int = {}, long = {}, float = {}, double = {}, boolean = {}",
+                c, b, s, i, l, f, d, bool);
     }
 
 }
