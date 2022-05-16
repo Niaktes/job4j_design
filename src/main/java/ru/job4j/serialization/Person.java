f56b33cf52d06d591aa920d1170fd8ac2872ade2
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2022-05-16 11:14:06 +0300

    2.2.4.4 Just explore JAXB.

--- src/main/java/ru/job4j/serialization/Person.java
+++ src/main/java/ru/job4j/serialization/Person.java
@@ -1,15 +1,30 @@
 package ru.job4j.serialization;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
 
-    private final boolean sex;
-    private final int age;
-    private final Contact contact;
-    private final String[] statuses;
+    @XmlAttribute
+    private boolean sex;
+    @XmlAttribute
+    private int age;
+    private Contact contact;
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "status")
+    private String[] statuses;
 
-    public Person(boolean sex, int age, Contact contact, String[] statuses) {
+    public Person() { }
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
         this.age = age;
         this.contact = contact;
@@ -24,6 +39,29 @@ public class Person {
                 + ", contact=" + contact
                 + ", statuses=" + Arrays.toString(statuses)
                 + "}";
+    }
+
+
+    public static void main(String[] args) throws JAXBException {
+        final Person person = new Person(false, 30, new Contact(123456, "11-111"), "Worker", "Married");
+
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        String result = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(result)) {
+            Person pers = (Person) unmarshaller.unmarshal(reader);
+            System.out.println(pers);
+        }
     }
 
 }
